108fd21bb14e8a9766643f8cf19f01fd67ddbf64
Brian Albarracín <baalbarracinb@unal.edu.co>
2025-06-21 11:45:10 -0500

    carrito de compra con pagina abierta

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -2,16 +2,20 @@ var currentPage = null;
 var defaultPage = "principal";
 var rootPath = "admin/";
 
-$(function(){
+$(function () {
 
-    
+
     const dataUser = localStorage.getItem("data-user");
     if (!dataUser) {
         console.warn("NO hay data-user en localStorage. Redirigiendo...");
         window.location.replace("index.html");
         return;
     }
-  
+
+
+    updateCartCount(user.id);
+
+
     //para que detecte la pagina de redireccionamiento
 
     const page = new URLSearchParams(window.location.search).get("page");
@@ -26,4 +30,19 @@ $(function(){
     loadFooter(rootPath);
     getPage("principal", rootPath)
 
+
 });
+
+function updateCartCount(userId) {
+    fetch(`https://irrigexback.onrender.com/cart/${userId}`)
+        .then(res => res.json())
+        .then(res => {
+            if (res.status !== 200) throw new Error("Error en la carga del carrito");
+
+            const totalQuantity = res.data.reduce((sum, item) => sum + item.quantity, 0);
+            $("#cart-count").text(totalQuantity);
+        })
+        .catch(err => {
+            console.error("❌ No se pudo actualizar el contador del carrito", err);
+        });
+}
